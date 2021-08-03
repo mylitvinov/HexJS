4c5debffc053a8ec7341b685469c1a4f76b0ccbe
mylitvinov <vinchester30@mail.ru>
2021-08-03 17:53:43 +0300

    add getLastWordLength.js

---
+++ HexTasks/ArrayTasks/getLastWordLength.js
@@ -0,0 +1,15 @@
+// Реализуйте и экспортируйте по умолчанию функцию, которая возвращает длину 
+// последнего слова переданной на вход строки. Словом считается любая последовательность,
+//  не содержащая пробелов.
+const getLastWordLength = (str) => {
+    if(!str.length) return 0;                // убираем лишние пробелы в начале и конце   
+    const strToArr = str.trim().split(' '); // и преобразуем в массив
+    return strToArr[strToArr.length -1].length; // находим дилну последнего индекса массива
+}
+
+
+ console.log(getLastWordLength('')); // 0
+ 
+console.log(getLastWordLength('man in BlacK')); // 5
+ 
+console.log(getLastWordLength('hello, world!  ')); // 6
