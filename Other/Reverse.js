32f60c41212c2c5d01b845ad1d55e00d5cc587cb
mylitvinov <vinchester30@mail.ru>
2021-08-06 15:51:21 +0300

    ad Reverse.js

--- Other/Reverse.js
+++ Other/Reverse.js
@@ -1,23 +1,33 @@
-const reverse = (str) => {
-    let i = str.length-1;
-    let rev = '';
-     while(i >= 0 ){
-       rev += str[i]
-       i--
-     }
-    return rev
+const revers2 = str2 => {
+  let revStr2 = '';
+  for (i = str2.length - 1; i >= 0; i--) {
+    revStr2 += str2[i];
+
   }
-
-  console.log(reverse('perevorot'));
-
-  // 2 способ
-  const revers2 = str2 =>{
-    let revStr2 = '';
-    for(i=str2.length-1; i>=0; i--){
-        revStr2 += str2[i];
-        
-    }
-    return revStr2
+  return revStr2
 }
 
+// С методами 
+const revers3 = (str) => {
+  return str
+  .toLowerCase()
+  .split('')
+  .reverse()
+  .join('')
+}
+
+
+// 
+const revers = (str) => {
+  let i = str.length - 1;
+  let rev = '';
+  while (i >= 0) {
+    rev += str[i]
+    i--
+  }
+  return rev
+}
+
+console.log(revers('perevorot'));
 console.log(revers2('Максим'));
+console.log(revers3('Максим'));
